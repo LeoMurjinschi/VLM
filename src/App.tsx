3e1cb2dc2af3fbb340bd8ea6eaa0826bcf332c9a
LeoMurjinschi <leomurjinschi@gmail.com>
2026-02-16 00:31:52 +0200

    Created Login and SignUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/public/LandingPage';
+import LoginPage from './pages/public/LoginPage';
+import SignUpPage from './pages/public/SignUpPage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
     </Router>
---
+++ src/pages/public/LoginPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../../components/layout/Navbar';
+
+const LoginPage = () => {
+    return (
+        <div className="min-h-screen bg-white font-sans text-gray-900">
+      <Navbar />
+      
+      </div>
+    
+    );
+};
+
+export default LoginPage;
