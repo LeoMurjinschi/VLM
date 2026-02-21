f347fa7948fe39e3e7e8a7009cfd237939a5122a
MihailAlexei <alexei55mihai@gmail.com>
2026-02-21 12:20:37 +0200

    created the card component for a donation

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom'; 
 import Sidebar from './Sidebar';
-import { Bars3Icon } from '@heroicons/react/24/outline';
 
 const DashboardLayout: React.FC = () => { 
   return (
@@ -15,12 +14,7 @@ const DashboardLayout: React.FC = () => {
       <div className="flex-1 flex flex-col overflow-hidden relative">
         
         
-        <header className="md:hidden bg-white border-b border-gray-200 p-4 flex items-center justify-between shadow-sm z-10">
-            <span className="font-bold text-xl text-teal-600">EcoFoodWaste</span>
-            <button className="p-2 rounded-md text-gray-500 hover:bg-gray-100">
-                <Bars3Icon className="w-6 h-6" />
-            </button>
-        </header>
+        
 
         
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50/50 p-4 md:p-8">
