375f77164892c9c6e0896ccecdb932de92ab16b7
vrodideal <vasile96.ro@gmail.com>
2026-05-10 17:39:33 +0300

    feat: add HealthController to API and regenerate build artifacts

---
+++ backend/VLM.API/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VLM.API.Controllers;
+
+    
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("check")]
+        public IActionResult Check() { 
+        return Ok("server is up to running");
+        }
+    }
+
+
