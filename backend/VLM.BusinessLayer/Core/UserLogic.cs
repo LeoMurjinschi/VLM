1119a2104d9e269073f2c76a3bdda51cae40d2fd
LeoMurjinschi <157521849+LeoMurjinschi@users.noreply.github.com>
2026-05-19 16:15:33 +0300

    Added UserLogic to BusinessLogic Core

---
+++ backend/VLM.BusinessLayer/Core/UserLogic.cs
@@ -0,0 +1,34 @@
+using VLM.BusinessLayer.Interface;
+using VLM.BusinessLayer.Structure;
+using VLM.Domain.Models.Service;
+using VLM.Domain.Models.User;
+
+namespace VLM.BusinessLayer.Core;
+
+public class UserLogic : UserActions, IUserLogic
+{
+    public ServiceResponse GetUserList()
+    {
+        return GetUserListAction();
+    }
+
+    public ServiceResponse GetUserById(int id)
+    {
+        return GetUserByIdAction(id);
+    }
+
+    public ServiceResponse CreateUser(UserCreateDto userCreateDto)
+    {
+        return CreateUserAction(userCreateDto);
+    }
+
+    public ServiceResponse UpdateUser(int id, UserCreateDto userCreateDto)
+    {
+        return UpdateUserAction(id, userCreateDto);
+    }
+
+    public ServiceResponse DeleteUser(int id)
+    {
+        return DeleteUserAction(id);
+    }
+}
