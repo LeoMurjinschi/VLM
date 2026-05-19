8e758119663921b8f924a076a58116186524508e
LeoMurjinschi <157521849+LeoMurjinschi@users.noreply.github.com>
2026-05-19 16:17:28 +0300

    Added CommentLogic and DonationLogic interfaces to BusinessLayer

---
+++ backend/VLM.BusinessLayer/Interface/ICommentLogic.cs
@@ -0,0 +1,13 @@
+using VLM.Domain.Models.Comment;
+using VLM.Domain.Models.Service;
+
+namespace VLM.BusinessLayer.Interface;
+
+public interface ICommentLogic
+{
+    ServiceResponse GetCommentsByDonation(int donationId);
+    ServiceResponse GetCommentById(int id);
+    ServiceResponse CreateComment(CommentCreateDto commentCreateDto);
+    ServiceResponse UpdateComment(int id, CommentCreateDto commentCreateDto);
+    ServiceResponse DeleteComment(int id);
+}
