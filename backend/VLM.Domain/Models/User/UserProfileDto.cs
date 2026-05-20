94dcc8af7480af08afbb0408de9fc3f024bab70c
MihailAlexei <alexei55mihai@gmail.com>
2026-05-20 10:12:25 +0300

    Added UserProfileEntity and UserProfileDto

---
+++ backend/VLM.Domain/Models/User/UserProfileDto.cs
@@ -0,0 +1,18 @@
+namespace VLM.Domain.Models.User;
+
+public class UserProfileDto
+{
+    public int UserId { get; set; }
+    public string Phone { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string OrgName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string MissionStatement { get; set; } = string.Empty;
+    public string OperatingHours { get; set; } = string.Empty;
+    public int OperatingRadius { get; set; }
+    public string AcceptedCategories { get; set; } = string.Empty;
+    public string TransportType { get; set; } = string.Empty;
+    public bool HasIndustrialStorage { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public bool Verified { get; set; }
+}
