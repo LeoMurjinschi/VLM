979f4729e0762d499a410318383d40b3774de28b
MihailAlexei <alexei55mihai@gmail.com>
2026-05-26 17:57:41 +0300

    Add authorization attributes to CommentController, MessageController, and NotificationController

--- backend/VLM.API/Controllers/MessageController.cs
+++ backend/VLM.API/Controllers/MessageController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VLM.BusinessLayer;
 using VLM.BusinessLayer.Interface;
@@ -7,6 +8,7 @@ namespace VLM.API.Controllers;
 
 [ApiController]
 [Route("api/messages")]
+[Authorize]
 public class MessageController : ControllerBase
 {
     private readonly IMessageLogic _messageLogic;
